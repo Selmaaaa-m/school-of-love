feddfcaa04044e086110d853865d5aacf1c10f90
selma <selma.charghadchi@gmal.com>
2025-03-04 14:36:20 +0330

    build error fixed

--- pages/home/homePage.tsx
+++ pages/home/homePage.tsx
@@ -17,43 +17,6 @@ export interface Props {
 export default async function HomePage(data: Props) {
     const pageData = data.data
 
-    // let resData = undefined;
-    // let page: GetPost | undefined = undefined;
-    // const url = "new"
-    // const type = "POST"
-
-    // try {
-    //     const response = await fetch(
-    //         process.env.NEXT_PUBLIC_BASE_URL + `/api/v1/client/web/getPost/${type}/${url}`,
-    //         {
-    //             method: "GET",
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //             },
-    //         }
-    //     );
-
-    //     console.log("--------------------------------");
-        
-    //     console.log("get post response: ", response);
-        
-
-    //     if (!response.ok) {
-    //         page = undefined
-    //         throw new Error("Network response was not ok");
-    //     }
-    //     resData = await response.json() as GetPost;
-
-    //     console.log(resData);
-        
-    //     page = resData
-    // } catch (err) {
-    //     console.error(err)
-    // }
-
-    // console.log("post info: ", data);
-
-
     return (
         <>
             <Logo />

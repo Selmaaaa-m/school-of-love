74aace43e6add59850124ba61bc3bda74f4129fb
Selma <selma.charghadchi@gmail.com>
2025-03-04 21:19:28 +0330

    post list api added

--- pages/home/homePage.tsx
+++ pages/home/homePage.tsx
@@ -8,12 +8,39 @@ import Questions from "@/features/questions/questions";
 import Supporters from "@/features/supporters/supporters";
 
 import { GetPage } from "@/api/getPage"
+import { GetPostList } from "@/api/getPostList";
 
 export interface Props {
     data: GetPage | undefined;
 }
 
-export default function HomePage({ data }: Props) {
+export default async function HomePage({ data }: Props) {
+
+    let postListData = undefined
+
+    try {
+        const response = await fetch(
+            process.env.NEXT_PUBLIC_BASE_URL + `/api/v1/client/web/getPostList`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                cache: "no-cache",
+            }
+        );
+
+
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        postListData = await response.json() as GetPostList;
+        console.log(postListData);
+        
+
+    } catch (err) {
+        console.error(err)
+    }
 
     const pageData = data;
 
